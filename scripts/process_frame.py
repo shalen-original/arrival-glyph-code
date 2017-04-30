b52df7e92907a4580912af5de7a7713733f9cd8a
Matteo Nardini <matteo.shalen@gmail.com>
2017-04-30 20:14:20 +0200

    Initial example of frame processing

---
+++ scripts/process_frame.py
@@ -0,0 +1,26 @@
+#
+# Process a single frame
+#
+
+import cv2
+
+def process_frame(frame):
+
+	intermediary = []
+
+	#intermediary.append(('original', frame));
+
+	# Convert to grayscale
+	frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+	#intermediary.append(('grayscale', frame))
+
+	# Adaptive tresholding
+	# In some cases performes better (Time.03.jpg, LouiseWritesHepto.03.jpg)
+	gauss = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C , cv2.THRESH_BINARY,201,2)
+	intermediary.append(('Adaptive Gauss Tresh', gauss))
+
+	# Otsu tresholding
+	treshold, otsu = cv2.threshold(frame, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	intermediary.append(('Otsu', otsu))
+
+	return frame, intermediary
